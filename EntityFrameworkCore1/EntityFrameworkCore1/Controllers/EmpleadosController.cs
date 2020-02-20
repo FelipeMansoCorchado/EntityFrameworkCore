68b2d2b782d37e22a320b85af6aabc6fc1b85d7b
FelipeMansoCorchado <48017865+FelipeMansoCorchado@users.noreply.github.com>
2020-02-20 13:36:44 +0100

    Add files via upload
    Proyecto en el cuál se realiza la instalación del paquete Entity Framework en una aplicación .NET Core.

---
+++ EntityFrameworkCore1/EntityFrameworkCore1/Controllers/EmpleadosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EntityFrameworkCore1.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntityFrameworkCore1.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        RepositoryEmpleados repo;
+        public EmpleadosController(RepositoryEmpleados repo)
+        {
+            this.repo = repo;
+        }
+        public IActionResult Index()
+        {
+            return View(this.repo.GetEmpleados());
+        }
+    }
+}
---
+++ EntityFrameworkCore1/EntityFrameworkCore1/Data/EntityFrameworkCoreContext.cs
@@ -0,0 +1,16 @@
+﻿using EntityFrameworkCore1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore1.Data
+{
+    public class EntityFrameworkCoreContext: DbContext
+    {
+        public EntityFrameworkCoreContext(DbContextOptions<EntityFrameworkCoreContext> options) : base(options) { }
+
+        public DbSet<Empleado> Empleados { get; set; }
+    }
+}
